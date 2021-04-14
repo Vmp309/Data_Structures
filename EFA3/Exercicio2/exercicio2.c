1621726c4934bb124acd03ef021dce58ec1969c5
Victoria Monteiro <52867523+Vmp309@users.noreply.github.com>
2021-04-14 11:29:18 -0300

    Add files via upload

---
+++ EFA3/Exercicio2/exercicio2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore_binaria.h"
+
+int main (){
+
+// cria a arvore
+Arv* a = arv_cria(
+    arv_criano(1,
+            arv_criano(2, NULL ,
+                    arv_criano(3, NULL , NULL)
+            ),
+
+        arv_criano(4,
+            arv_criano(5, NULL , NULL), arv_criano(6, NULL , NULL)
+        )
+
+    )
+
+);
+
+/*Printa a quantidade de folhas*/
+printf("Quantidade de folhas na arvore: %d\n",  folhas(a));
+
+
+/*Imprime os nohs da arvore*/
+arv_imprime(a);
+
+
+arv_libera(a);
+
+return 0;
+}
